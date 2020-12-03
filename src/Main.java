147141387fe69c6e40b3ae89f90a9361b1db9650
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-03 17:47:56 +0200

    remove tester code from main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
-import java.util.List;
-
 public class Main {
   public static void main(String[] args) {
-    // test Project class
-    User assignee = new User();
-    User supervisor = new User();
-    Project project = new Project.Builder("washing the dishes")
-                          .addAssignee(assignee)
-                          .addSupervisor(supervisor)
-                          .build();
+
   }
 }
