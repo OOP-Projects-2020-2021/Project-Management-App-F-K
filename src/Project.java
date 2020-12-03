4ebb3d7fe2cf66d4a889cf5ed63c4b5910f77c4c
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-03 17:03:25 +0200

    create builder for project

--- src/Project.java
+++ src/Project.java
@@ -1,6 +1,8 @@
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Project {
     public enum ProjectStatus {
@@ -16,4 +18,47 @@ public class Project {
     private @Nullable User supervisor;
     private List<User> contributors;
     private ProjectStatus status;
+
+    public static final class Builder {
+        private String name;
+        private @Nullable String description;
+        private @Nullable User assignee;
+        private @Nullable User supervisor;
+        private List<User> contributors;
+        private ProjectStatus status;
+
+        public Builder(String name) {
+            this.name = Objects.requireNonNull(name);
+            contributors = new ArrayList<>();
+        }
+
+        public Builder addDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder addAssignee(User assignee) {
+            this.assignee = assignee;
+            return this;
+        }
+
+        public Builder addSupervisor(User supervisor) {
+            this.supervisor = supervisor;
+            return this;
+        }
+
+        public Builder addContributor(User contributor) {
+            this.contributors.add(contributor);
+            return this;
+        }
+
+        public Builder setStatus(ProjectStatus status) {
+            this.status = status;
+            return this;
+        }
+
+//        public Project build() {
+//            // return new Project(this);
+//        }
+    }
 }
