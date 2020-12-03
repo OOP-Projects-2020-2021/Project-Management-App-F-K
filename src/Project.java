46cfeff6313cd2b4e6647c3b2910dba1b240fffe
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-03 16:53:53 +0200

    create ProjectStatus enum

---
+++ src/Project.java
@@ -0,0 +1,12 @@
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Project {
+    public enum ProjectStatus {
+        TO_DO,
+        IN_PROGRESS,
+        MARKED_AS_DONE,
+        FINISHED
+    }
+}
