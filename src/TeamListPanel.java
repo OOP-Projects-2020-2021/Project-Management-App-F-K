b298ec03fa57055e85d9d03b3c9210f171f5301a
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-06 23:58:03 +0200

    use modified flow layout for team panel

--- src/TeamListPanel.java
+++ src/TeamListPanel.java
@@ -6,27 +6,10 @@ public class TeamListPanel extends JPanel {
     private TeamListController controller = new TeamListController();
 
     public TeamListPanel() {
+        this.setLayout(new ModifiedFlowLayout());
         List<TeamViewModel> teams = controller.getUsersTeams();
         for (TeamViewModel team : teams) {
             this.add(new TeamLabel(team));
         }
     }
-
-//    @Override
-//    public Dimension getPreferredSize() {
-//        int col, row, h, w;
-//        Dimension d = null;
-//        w = (int) sb.getViewport().getSize().getWidth();
-//        col = w / chartwidth;
-//        if (col > 0) {
-//            row = (int)(Math.ceil(24.0/col));
-//            h = row * chartwidth;
-//            d = new Dimension(w, h);
-//        }
-//        else{
-//            d = super.getPreferredSize();
-//        }
-//        return d;
-//    }
-
 }
