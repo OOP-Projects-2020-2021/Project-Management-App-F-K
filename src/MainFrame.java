f91eddd41a12c16da54e5ebd2713d3c943f32a5b
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-06 14:51:28 +0200

    add mainframe

---
+++ src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        this.setTitle("Project Management App");
+        this.setJMenuBar(new MainMenu());
+        this.setSize(600, 400);
+        this.setVisible(true);
+    }
+}
