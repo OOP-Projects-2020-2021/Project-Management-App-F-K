cb8d41ab197fc801546f67911ed1b9688e476960
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-07 00:05:15 +0200

    add dummy data to teams list

--- src/TeamListController.java
+++ src/TeamListController.java
@@ -10,12 +10,14 @@ public class TeamListController {
                 Arrays.asList(new TeamViewModel("IT Team", "456238", "Andrew Smith"),
                                 new TeamViewModel("HR Team", "654236", "Maria Spencer"),
                                 new TeamViewModel("Entire Team", "656325", "Margaret Grant"),
-                                new TeamViewModel("IT Team2", "456238", "Andrew Smith"),
-                                new TeamViewModel("HR Team2", "654236", "Maria Spencer"),
-                                new TeamViewModel("Entire Team2", "656325", "Margaret Grant"),
-                                new TeamViewModel("IT Team3", "456238", "Andrew Smith"),
-                                new TeamViewModel("HR Team3", "654236", "Maria Spencer"),
-                                new TeamViewModel("Entire Team3", "656325", "Margaret Grant"))
+                                new TeamViewModel("Football team", "452238", "Greta Williams"),
+                                new TeamViewModel("Basketball Team", "123236", "Adrian Brown"),
+                                new TeamViewModel("Street Dance Team", "965525", "Abigail Johnson"),
+                                new TeamViewModel("Ballet team", "456628", "Bailey Miller"),
+                                new TeamViewModel("Handball team", "634236", "Casey Lewis"),
+                                new TeamViewModel("Volleyball team", "614125", "Edmund Robinson"),
+                                new TeamViewModel("Kayaking Team", "695625", "Howard Young"),
+                                new TeamViewModel("Rugby Team", "236325", "Katherine Phillips"))
         );
         return teams;
     }
