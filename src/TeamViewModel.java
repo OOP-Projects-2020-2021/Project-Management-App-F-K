a4ebb7005a28987e1016f3329ddb54c935c07a94
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-06 15:00:10 +0200

    create TeamVideModel class

---
+++ src/TeamViewModel.java
@@ -0,0 +1,23 @@
+public class TeamViewModel {
+    private String name;
+    private String code;
+    private String managerName;
+
+    public TeamViewModel(String name, String code, String managerName) {
+        this.name = name;
+        this.code = code;
+        this.managerName = managerName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+}
