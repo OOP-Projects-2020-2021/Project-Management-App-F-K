60f23ce8e1422bb68b2836bba3eb2017623d26b4
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-06 23:58:23 +0200

    add scrollable team panel to mainframe

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
+        this.add(new JScrollPane(new TeamListPanel()));
         this.setTitle("Project Management App");
         this.setJMenuBar(new MainMenu());
         this.setSize(600, 400);
