291820b0cb3c0fce02ffae586f43454a91897870
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-06 14:32:02 +0200

    create controller for main menu

---
+++ src/MainMenuController.java
@@ -0,0 +1,17 @@
+public class MainMenuController {
+    public void logoutUser() {
+        // todo
+    }
+
+    public void enableUserDataSettings() {
+        // todo
+    }
+
+    public void enableCreatingNewTeam() {
+        new CreateTeamFrame();
+    }
+
+    public void enableJoiningNewTeam() {
+        new JoinTeamFrame();
+    }
+}
