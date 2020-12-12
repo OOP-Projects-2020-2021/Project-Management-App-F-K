e0c6ad30549182a1320bf2304c67cc57b04de2ed
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-12 11:47:34 +0200

    sketch TeamRepository interface and sqlite implementation

--- src/main/java/model/team/repository/TeamRepository.java
+++ src/main/java/model/team/repository/TeamRepository.java
@@ -1,4 +1,16 @@
 package main.java.model.team.repository;
 
-public class TeamRepository {
+import main.java.model.User;
+import main.java.model.team.Team;
+
+public interface TeamRepository {
+    void createTeam(Team team);
+
+    void getTeam(String code);
+
+    void deleteTeam(Team team);
+
+    void joinTeam(User user, String teamCode);
+
+    void leaveTeam(User user, Team team);
 }
---
+++ src/main/java/model/team/repository/impl/SqliteTeamRepository.java
@@ -0,0 +1,32 @@
+package main.java.model.team.repository.impl;
+
+import main.java.model.User;
+import main.java.model.team.Team;
+import main.java.model.team.repository.TeamRepository;
+
+public class SqliteTeamRepository implements TeamRepository {
+    @Override
+    public void createTeam(Team team) {
+
+    }
+
+    @Override
+    public void getTeam(String code) {
+
+    }
+
+    @Override
+    public void deleteTeam(Team team) {
+
+    }
+
+    @Override
+    public void joinTeam(User user, String teamCode) {
+
+    }
+
+    @Override
+    public void leaveTeam(User user, Team team) {
+
+    }
+}
