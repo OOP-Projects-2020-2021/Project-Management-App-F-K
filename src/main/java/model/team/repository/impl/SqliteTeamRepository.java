fc0b80403998649cfb7e2d972b2a7a2078db9321
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-12 12:57:31 +0200

    extract queries and stataments as constants

--- src/main/java/model/team/repository/impl/SqliteTeamRepository.java
+++ src/main/java/model/team/repository/impl/SqliteTeamRepository.java
@@ -4,19 +4,38 @@ import main.java.model.User;
 import main.java.model.team.Team;
 import main.java.model.team.repository.TeamRepository;
 
+import javax.print.DocFlavor;
 import java.sql.*;
 
 public class SqliteTeamRepository implements TeamRepository {
     private Connection c;
 
+    private static final String GET_TEAM_WITH_CODE_QUERY = "Select * from Team WHERE Code = ?";
+    private PreparedStatement getTeamWithCode;
+    private static final String ADD_TEAM_MEMBERSHIP_QUERY = "INSERT Into MemberToTeam (MemberId, " +
+            "TeamId) VALUES (?, (SELECT TeamId " +
+            "FROM Team WHERE Code = ?))";
+    private PreparedStatement addTeamMembership;
+    private static final String REMOVE_TEAM_MEMBERSHIP_QUERY = "DELETE FROM MemberToTeam WHERE  " +
+            "MemberId = ? AND TeamId = (SELECT TeamId " +
+            "FROM Team WHERE Code = ?)";
+    private PreparedStatement removeTeamMembership;
+
+
     SqliteTeamRepository() {
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:project_management_app.db");
-            System.out.println("Succesful database connection");
+            prepareStatements();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private void prepareStatements() throws SQLException {
+        getTeamWithCode = c.prepareStatement(GET_TEAM_WITH_CODE_QUERY);
+        addTeamMembership = c.prepareStatement(ADD_TEAM_MEMBERSHIP_QUERY);
+        removeTeamMembership = c.prepareStatement(REMOVE_TEAM_MEMBERSHIP_QUERY);
     }
 
     @Override
@@ -26,10 +45,8 @@ public class SqliteTeamRepository implements TeamRepository {
 
     @Override
     public Team getTeam(String code) throws SQLException {
-        String query = "Select * from Team WHERE Code = ?";
-        PreparedStatement statement = c.prepareStatement(query);
-        statement.setString(1, code);
-        ResultSet result = statement.executeQuery();
+        getTeamWithCode.setString(1, code);
+        ResultSet result = getTeamWithCode.executeQuery();
         result.next();
         int id = result.getInt("TeamId");
         String teamName = result.getString("TeamName");
@@ -45,24 +62,16 @@ public class SqliteTeamRepository implements TeamRepository {
 
     @Override
     public void joinTeam(User user, String teamCode) throws SQLException {
-        String query = "INSERT Into MemberToTeam (MemberId, TeamId) VALUES (?, (SELECT TeamId " +
-                "FROM Team WHERE Code = ?))";
-        PreparedStatement statement = c.prepareStatement(query);
-        // todo
-        statement.setInt(1, 4);
-        statement.setString(2, teamCode);
-        statement.execute();
+        addTeamMembership.setInt(1, 5);
+        addTeamMembership.setString(2, teamCode);
+        addTeamMembership.execute();
     }
 
     @Override
     public void leaveTeam(User user, Team team) throws SQLException {
-        String query = "DELETE FROM MemberToTeam WHERE MemberId = ? AND TeamId = (SELECT TeamId " +
-                "FROM Team WHERE Code = ?)";
-        PreparedStatement statement = c.prepareStatement(query);
-        // todo
-        statement.setInt(1, 3);
-        statement.setString(2, team.getCode());
-        statement.execute();
+        removeTeamMembership.setInt(1, 1);
+        removeTeamMembership.setString(2, team.getCode());
+        removeTeamMembership.execute();
     }
 
     public static void main(String[] args) throws SQLException {
@@ -70,5 +79,7 @@ public class SqliteTeamRepository implements TeamRepository {
         Team team = repository.getTeam("895621");
         System.out.println("Team with name " + team.getName() + " and id " + team.getId());
         repository.joinTeam(new User("Anna", "pass"), "895621");
+        repository.leaveTeam(new User("Anna", "pass"), new Team(1, "aa", new User("", ""),
+                "895621"));
     }
 }
