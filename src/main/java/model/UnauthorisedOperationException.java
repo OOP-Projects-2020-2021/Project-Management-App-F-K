c1146bcdaeeb4e2c51f5a742edeb79f368c09545
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-12 23:41:38 +0200

    create UnauthorisedOperationException

---
+++ src/main/java/model/UnauthorisedOperationException.java
@@ -0,0 +1,8 @@
+package model;
+
+public class UnauthorisedOperationException extends Exception {
+    public UnauthorisedOperationException(int userId, String operation, String reason) {
+        super("The user with id " + userId + " is not authorised to perform " + operation  + " " +
+                "because " + reason);
+    }
+}
