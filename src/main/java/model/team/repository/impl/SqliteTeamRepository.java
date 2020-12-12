daec30b683263a656a7fdfae78dadc191afe99e4
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-12 12:19:44 +0200

    implement getTeam

--- src/main/java/model/team/repository/impl/SqliteTeamRepository.java
+++ src/main/java/model/team/repository/impl/SqliteTeamRepository.java
@@ -4,20 +4,15 @@ import main.java.model.User;
 import main.java.model.team.Team;
 import main.java.model.team.repository.TeamRepository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SqliteTeamRepository implements TeamRepository {
     private Connection c;
-    private Statement statement;
 
     SqliteTeamRepository() {
         try {
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:project_management_app.db");
-            statement = c.createStatement();
             System.out.println("Succesful database connection");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
@@ -30,8 +25,17 @@ public class SqliteTeamRepository implements TeamRepository {
     }
 
     @Override
-    public void getTeam(String code) {
-
+    public Team getTeam(String code) throws SQLException {
+        String query = "Select * from Team WHERE Code = ?";
+        PreparedStatement statement = c.prepareStatement(query);
+        statement.setString(1, code);
+        ResultSet result = statement.executeQuery();
+        result.next();
+        int id = result.getInt("TeamId");
+        String teamName = result.getString("TeamName");
+        int managerId = result.getInt("ManagerId");
+        String teamCode = result.getString("Code");
+        return new Team(id, teamName, new User("", ""), teamCode);
     }
 
     @Override
@@ -49,7 +53,9 @@ public class SqliteTeamRepository implements TeamRepository {
 
     }
 
-    public static void main(String[] args) {
-        new SqliteTeamRepository();
+    public static void main(String[] args) throws SQLException {
+        TeamRepository repository = new SqliteTeamRepository();
+        Team team = repository.getTeam("895621");
+        System.out.println("Team with name " + team.getName() + " and id " + team.getId());
     }
 }
