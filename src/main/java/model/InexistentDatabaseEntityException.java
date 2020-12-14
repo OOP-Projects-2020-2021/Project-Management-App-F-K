de3075b801e6306744e58f7e2da53c1769805e3d
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-14 22:06:10 +0200

    create InexistentDatabaseEntityExtension

---
+++ src/main/java/model/InexistentDatabaseEntityException.java
@@ -0,0 +1,9 @@
+package model;
+
+import model.project.Project;
+
+public class InexistentDatabaseEntityException extends Exception {
+    public InexistentDatabaseEntityException(Project.SavableProject p) {
+        super ("SaveableProjects do not exist in the database yet, so id access is illegal");
+    }
+}
