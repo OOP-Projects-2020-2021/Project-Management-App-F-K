15bcbc3f430ce5bba06224db6830cba3c2b4798a
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-14 23:13:36 +0200

    create ProjectManager

---
+++ src/main/java/model/project/ProjectManager.java
@@ -0,0 +1,15 @@
+package model.project;
+
+import model.project.repository.ProjectRepository;
+import model.project.repository.impl.SqliteProjectRepository;
+
+public class ProjectManager {
+    private static ProjectManager instance = new ProjectManager();
+    private ProjectRepository projectRepository = new SqliteProjectRepository();
+
+    private ProjectManager() {}
+
+    public static ProjectManager getInstance() {
+        return instance;
+    }
+}
