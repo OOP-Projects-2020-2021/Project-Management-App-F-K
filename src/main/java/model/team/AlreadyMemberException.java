3b34847a56125088ac27077bd34ba1707ea9dbd3
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-15 19:44:33 +0200

    create AlreadyMemberException

---
+++ src/main/java/model/team/AlreadyMemberException.java
@@ -0,0 +1,8 @@
+package model.team;
+
+public class AlreadyMemberException extends Exception {
+    AlreadyMemberException(String userName, String teamName) {
+        super("The user " + userName + " can't join " + teamName + " team, because the user is " +
+                "already a member");
+    }
+}
