8bdb10393b7188c09e081bdfaa716f83b7dfd57f
keresztesbeata <keresztesbeata00@yahoo.com>
2020-12-17 11:33:44 +0200

    members list added

--- src/main/java/view/project/TeamMembersPanel.java
+++ src/main/java/view/project/TeamMembersPanel.java
@@ -9,67 +9,94 @@ import java.awt.event.ActionListener;
 
 public class TeamMembersPanel extends JPanel implements ActionListener {
 
-    private JComboBox<String> membersList;
-
-    private JLabel membersLabel;
-    private JLabel removeMemberLabel;
     private JLabel addMemberLabel;
     private JButton addMemberButton;
     private JButton removeMemberButton;
     private JTextField addMemberTextField;
+    private JList<String> membersList;
 
     public TeamMembersPanel(Dimension parentFrameDimension) {
         this.setPreferredSize(parentFrameDimension);
-        initMembersPane();
+        this.setLayout(new BorderLayout());
+        initMembersPane(true);  // todo get form controller
+
     }
-    private void initMembersPane() {
-        GroupLayout membersLayout = new GroupLayout(this);
+    private void initMembersPane(boolean enableManagingMembers) {
+        initComponents();
+        initMembersHeader(enableManagingMembers);
+        initMembersContent(enableManagingMembers);
+    }
+
+    private void initMembersHeader(boolean enableManagingMembers) {
+        JPanel headerPanel = new JPanel();
+        GroupLayout membersLayout = new GroupLayout(headerPanel);
         membersLayout.setAutoCreateGaps(true);
         membersLayout.setAutoCreateContainerGaps(true);
-        this.setLayout(membersLayout);
-        initComponents();
-        initMembersList();
-
+        headerPanel.setLayout(membersLayout);
         membersLayout.setHorizontalGroup(membersLayout.createSequentialGroup()
-                .addGroup(membersLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(membersLabel)
-                        .addComponent(removeMemberLabel)
+                .addGroup(membersLayout.createParallelGroup()
                         .addComponent(addMemberLabel))
-                .addGroup(membersLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(membersList)
-                        .addComponent(addMemberTextField))
-                .addGroup(membersLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                        .addComponent(removeMemberButton)
-                        .addComponent(addMemberButton)));
-
+                        .addComponent(addMemberTextField)
+                        .addComponent(addMemberButton));
         membersLayout.setVerticalGroup(membersLayout.createSequentialGroup()
-                .addGroup(membersLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(membersLabel))
-                .addGroup(membersLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                        .addComponent(removeMemberLabel)
-                        .addComponent(membersList)
-                        .addComponent(removeMemberButton))
                 .addGroup(membersLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                         .addComponent(addMemberLabel)
                         .addComponent(addMemberTextField)
                         .addComponent(addMemberButton)));
+        this.add(headerPanel,BorderLayout.NORTH);
+        headerPanel.setVisible(enableManagingMembers);
+    }
+    private void initMembersContent(boolean enableManagingMembers) {
+
+        JPanel contentPanel = new JPanel();
+        contentPanel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        GroupLayout contentPanelLayout = new GroupLayout(contentPanel);
+        contentPanel.setLayout(contentPanelLayout);
+        contentPanelLayout.setAutoCreateGaps(true);
+        contentPanelLayout.setAutoCreateContainerGaps(true);
+
+        initMembersList();
+        JPanel membersListPanel = new JPanel();
+        JScrollPane listScroller = new JScrollPane(membersListPanel);
+        listScroller.setViewportView(membersList);
+        listScroller.setVisible(true);
+        membersListPanel.add(listScroller);
+
+        contentPanelLayout.setHorizontalGroup(contentPanelLayout.createSequentialGroup()
+                    .addComponent(listScroller)
+                    .addComponent(removeMemberButton));
+        contentPanelLayout.setVerticalGroup(contentPanelLayout.createSequentialGroup()
+                    .addGroup(contentPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                            .addComponent(listScroller)
+                            .addComponent(removeMemberButton)));
+        this.add(contentPanel,BorderLayout.CENTER);
+
+        removeMemberButton.setVisible(enableManagingMembers);
     }
     private void initComponents() {
-        membersLabel = UIFactory.createLabel("Members:",null);
-        removeMemberLabel = UIFactory.createLabel("Remove member:",null);
         addMemberLabel = UIFactory.createLabel("Add member:",null);
         addMemberTextField = UIFactory.createTextField(""); //todo check if empty
         addMemberButton = UIFactory.createButton("Add");
         removeMemberButton = UIFactory.createButton("Remove");
     }
     private void initMembersList() {
-        String[] members = {"User1","User2","User3","User4","User5"};
-        membersList = new JComboBox<>(members);
+        int noMembers = 20;
+        String[] members = new String[noMembers];
+        for(int i=0;i<noMembers;i++) {
+            members[i] = "User" + i;
+        }
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        for (String member : members) {
+            listModel.addElement(member);
+        }
+        membersList = new JList<>(listModel);
+        membersList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        membersList.setLayoutOrientation(JList.VERTICAL);
 
     }
-    public void enableButtons(boolean enableButton) {
-        addMemberButton.setVisible(enableButton);
-        removeMemberButton.setVisible(enableButton);
+    public void enableManagingMembers(boolean enableManager) {
+        addMemberButton.setVisible(enableManager);
+        removeMemberButton.setVisible(enableManager);
     }
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
