b59b49e472837b4c735d0be1e6edebb73e6d6bed
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-17 18:23:05 +0200

    add animation to TeamLabels

--- src/main/java/view/team/TeamLabel.java
+++ src/main/java/view/team/TeamLabel.java
@@ -28,6 +28,10 @@ public class TeamLabel extends JLabel {
 
   private TeamLabelController controller;
 
+  private static final int THIN_BORDER_THICKNESS = 1;
+  private static final int THICKER_BORDER_THICKNESS = 2;
+  private static final int VERY_THICK_BORDER_THICKNESS = 3;
+
   public TeamLabel(JFrame frame, TeamViewModel teamData) {
     super(
         String.format(
@@ -37,10 +41,24 @@ public class TeamLabel extends JLabel {
             teamData.getManagerName()));
     this.controller = new TeamLabelController(teamData.getId(), frame);
     this.setPreferredSize(LABEL_DIMENSION);
-    this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+    this.setThinBorder();
     this.addMouseListener(new TeamLabelMouseListener());
   }
 
+  private void setThinBorder() {
+    this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, THIN_BORDER_THICKNESS));
+    this.revalidate();
+  }
+
+  private void setThickerBorder() {
+    this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, THICKER_BORDER_THICKNESS));
+    this.revalidate();
+  }
+
+  private void setVeryThickBorder() {
+    this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY, VERY_THICK_BORDER_THICKNESS));
+    this.revalidate();
+  }
 
   private class TeamLabelMouseListener implements MouseListener {
 
@@ -51,22 +69,22 @@ public class TeamLabel extends JLabel {
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
-
+      setVeryThickBorder();
     }
 
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
-
+      setThickerBorder();
     }
 
     @Override
     public void mouseEntered(MouseEvent mouseEvent) {
-
+      setThickerBorder();
     }
 
     @Override
     public void mouseExited(MouseEvent mouseEvent) {
-
+      setThinBorder();
     }
   }
 }
