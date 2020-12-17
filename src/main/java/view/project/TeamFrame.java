bc4246ad56acbccfd957ac05d4cae62076b0710c
keresztesbeata <keresztesbeata00@yahoo.com>
2020-12-17 11:34:53 +0200

    remove main menu

--- src/main/java/view/project/TeamFrame.java
+++ src/main/java/view/project/TeamFrame.java
@@ -38,43 +38,12 @@ public class TeamFrame extends JFrame implements ActionListener {
         this.parentFrame = parentFrame;
         this.setSize(DIMENSION);
         this.setResizable(false);
-        initMenuComponents();
         this.setJMenuBar(teamMenuBar);
-        // add components here
         addTabbedPanes();
-        //this.pack();
         this.addWindowListener(new projectWindowAdapter());
         this.setVisible(true);
     }
 
-    private void initMenuComponents() {
-        teamMenuBar = new JMenuBar();
-
-        teamMenu = new JMenu("Team Menu");
-        teamMenu.setPreferredSize(new Dimension(100,25));
-
-        teamSettingsItem = new JMenuItem("Team Settings");
-        projectListItem = new JMenuItem("Projects");
-        leaveTeamItem = new JMenuItem("Leave team");
-        backItem = new JMenuItem("Back");
-
-        teamSettingsItem.setMnemonic(KeyEvent.VK_S);
-        projectListItem.setMnemonic(KeyEvent.VK_P);
-        leaveTeamItem.setMnemonic(KeyEvent.VK_L);
-        backItem.setMnemonic(KeyEvent.VK_B);
-
-        teamSettingsItem.addActionListener(this);
-        projectListItem.addActionListener(this);
-        leaveTeamItem.addActionListener(this);
-        backItem.addActionListener(this);
-
-        teamMenu.add(teamSettingsItem);
-        teamMenu.add(projectListItem);
-        teamMenu.add(leaveTeamItem);
-        teamMenu.add(backItem);
-
-        teamMenuBar.add(teamMenu);
-    }
     private void initTabbedPanes() {
         homeTab = new TeamHomePanel(DIMENSION);
         membersTab = new TeamMembersPanel(DIMENSION);
