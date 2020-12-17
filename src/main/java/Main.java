aec296fbd311f4398cb58d3add8cb506d7bca784
keresztesbeata <keresztesbeata00@yahoo.com>
2020-12-17 16:25:55 +0200

    deleted testing code from main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
+import view.user.SignInFrame;
+
 import javax.swing.*;
-import view.team.single_team.TeamFrame;
 
 public class Main {
   public static void main(String[] args) {
-    // JFrame signInFrame = new SignInFrame();
-    //JFrame home = new TeamSettingsFrame();
-    JFrame home = new TeamFrame(new JFrame());
+    JFrame signInFrame = new SignInFrame();
   }
 }
