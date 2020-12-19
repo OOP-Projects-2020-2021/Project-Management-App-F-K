f81a6e24081319ca1adbca9329e13d4e14597148
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-20 00:27:59 +0200

    add javadoc to QueryProjectDeadlineStatus

--- src/main/java/model/project/queryconstants/QueryProjectDeadlineStatus.java
+++ src/main/java/model/project/queryconstants/QueryProjectDeadlineStatus.java
@@ -1,7 +1,11 @@
 package model.project.queryconstants;
 
+/**
+ * QueryProjectDeadlineStatus is used for database queries and specifies the required status of
+ * the project with respect to its deadline.
+ */
 public enum QueryProjectDeadlineStatus {
-    ALL,
-    IN_TIME,
-    OVERDUE
+    ALL, //no specified status.
+    IN_TIME, //the deadline is later than the current date, or today.
+    OVERDUE //the deadline is earlier than the current date.
 }
