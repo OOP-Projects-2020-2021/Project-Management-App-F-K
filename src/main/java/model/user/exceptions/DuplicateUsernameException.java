cd32bf9fc7c89ea2f79cd8eeacdbe60a81440978
keresztesbeata <keresztesbeata00@yahoo.com>
2020-12-20 15:53:53 +0200

    added javaDoc

--- src/main/java/model/user/exceptions/DuplicateUsernameException.java
+++ src/main/java/model/user/exceptions/DuplicateUsernameException.java
@@ -1,5 +1,11 @@
 package model.user.exceptions;
 
+/**
+ * DuplicateUsernameException is thrown when a user attempts to sign-in with an already existing username
+ * in the database.
+ *
+ * @author Beata Keresztes
+ */
 public class DuplicateUsernameException extends Exception {
 
   public DuplicateUsernameException(String userName) {
