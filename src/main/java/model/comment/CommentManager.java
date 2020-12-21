8214bfd528d97824b38e56148cf507badce1d718
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-22 00:20:43 +0200

    add javadoc to commentmanager

--- src/main/java/model/comment/CommentManager.java
+++ src/main/java/model/comment/CommentManager.java
@@ -15,6 +15,14 @@ import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * CommentManager is responsible for executing all the commands needed for the application that are
+ * related to comments.
+ *
+ * <p>Remark that it is implemented with the singleton pattern, so only one instance of it exists.
+ *
+ * @author Bori Fazakas
+ */
 public class CommentManager extends Manager {
   private static CommentManager instance = new CommentManager();
 
@@ -24,6 +32,20 @@ public class CommentManager extends Manager {
     return instance;
   }
 
+  /**
+   * Adds a new comment to project with id projectId, from the current user, with the content text.
+   *
+   * @param text is the content of the new comment.
+   * @param projectId is the id of the project to which it will belong.
+   * @throws NoSignedInUserException if there is noone signed in.
+   * @throws InexistentProjectException if there is no project with id projectId.
+   * @throws SQLException if the operation could  not be performed in the database.
+   * @throws InexistentDatabaseEntityException should never occur.
+   * @throws InexistentTeamException if the team wo which the project belongs is not found in the
+   * database.
+   * @throws UnauthorisedOperationException if the current user is not the member of the team to
+   * which the project with projectId belongs.
+   */
   public void addComment(String text, int projectId)
       throws NoSignedInUserException, InexistentProjectException, SQLException,
           InexistentDatabaseEntityException, InexistentTeamException,
@@ -40,6 +62,13 @@ public class CommentManager extends Manager {
     commentRepository.saveComment(comment);
   }
 
+  /**
+   * Finds and returns from the database a list of all comments belonging to a a project.
+   *
+   * @param projectId is the id of the project whose comments are listed.
+   * @return a list of all comments belonging to a a project.
+   * @throws SQLException if the operation could  not be performed in the database.
+   */
   public List<Comment> getOrderedCommentsOfProject(int projectId) throws SQLException {
     List<Comment> comments = commentRepository.getCommentsOfProject(projectId);
     comments.sort(Comparator.comparing(Comment::getDateTime));
