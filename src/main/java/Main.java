e541ef36675addc670f5ffae2b082b22fe910a72
keresztesbeata <keresztesbeata00@yahoo.com>
2020-12-21 16:15:07 +0200

    removed testing code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,9 @@
-import model.user.UserManager;
-import view.team.single_team.TeamFrame;
 import view.user.SignInFrame;
 
 import javax.swing.*;
-import java.sql.SQLException;
 
 public class Main {
   public static void main(String[] args) {
-   // JFrame signInFrame = new SignInFrame();
-    UserManager u = UserManager.getInstance();
-    try{
-      u.signIn("bea","bea");
-    }catch(SQLException e) {
-      System.out.println("not signed in");
-    }
-    JFrame t = new TeamFrame(new JFrame(),52);
+    JFrame signInFrame = new SignInFrame();
   }
 }
