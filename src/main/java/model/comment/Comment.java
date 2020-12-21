2187c78546ea61a96d9b5bdd514da55455a382df
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-22 00:15:25 +0200

    add javadoc to Comment

--- src/main/java/model/comment/Comment.java
+++ src/main/java/model/comment/Comment.java
@@ -4,13 +4,22 @@ import model.InexistentDatabaseEntityException;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a Comment belonging to a project and holds its data. Immutable.
+ *
+ * @author Bori Fazakas
+ */
 public class Comment {
-  private final int id;
+  private final int id; //the id in the database.
   private final String text;
-  private final int projectId;
-  private final int senderId;
-  private final LocalDateTime dateTime;
+  private final int projectId; //the id of the project to which it belongs.
+  private final int senderId; //the id of the user who sent it.
+  private final LocalDateTime dateTime; //the time when it was sent.
 
+  /**
+   * This class is used only when the comment instance is created to be saved in the database, but
+   * does not have a valid id yet.
+   */
   public static class SavableComment extends Comment {
 
     public SavableComment(String text, int projectId, int senderId, LocalDateTime dateTime) {
