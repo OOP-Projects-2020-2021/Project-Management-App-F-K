200ed0791ca6ab1ef67df8d0f678f40b33d46eda
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-27 22:43:21 +0200

    add functions for status change in ProjectStatusController

--- src/main/java/controller/project/single_project/ProjectStatusController.java
+++ src/main/java/controller/project/single_project/ProjectStatusController.java
@@ -2,10 +2,12 @@ package controller.project.single_project;
 
 import controller.FrameController;
 import model.InexistentDatabaseEntityException;
+import model.UnauthorisedOperationException;
 import model.project.Project;
 import model.project.ProjectManager;
 import model.project.exceptions.IllegalProjectStatusChangeException;
 import model.project.exceptions.InexistentProjectException;
+import model.user.exceptions.NoSignedInUserException;
 import view.ErrorDialogFactory;
 
 import javax.swing.*;
@@ -27,6 +29,62 @@ public class ProjectStatusController extends ProjectController {
             ErrorDialogFactory.createErrorDialog(exception, frame, null);
         } catch (IllegalProjectStatusChangeException e) {
             ErrorDialogFactory.createErrorDialog(e, frame, "You can't mark a project with status " + getProject().getStatus().toString() + " as 'In Progress'");
+        }
+    }
+
+    public void setBackToToDo() {
+        try {
+            projectManager.setProjectAsToDo(getProject().getId());
+        } catch (SQLException | InexistentDatabaseEntityException | InexistentProjectException e) {
+            Exception exception = new SQLException();
+            ErrorDialogFactory.createErrorDialog(exception, frame, null);
+        } catch (IllegalProjectStatusChangeException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, "You can't mark a project with status " + getProject().getStatus().toString() + " as 'In Progress'");
+        } catch (NoSignedInUserException | UnauthorisedOperationException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, null);
+        }
+    }
+
+    public void turnIn() {
+        try {
+            projectManager.turnInProject(getProject().getId());
+        } catch (SQLException | InexistentDatabaseEntityException | InexistentProjectException e) {
+            Exception exception = new SQLException();
+            ErrorDialogFactory.createErrorDialog(exception, frame, null);
+        } catch (IllegalProjectStatusChangeException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, "You can't mark a project with status " + getProject().getStatus().toString() + " as 'In Progress'");
+        } catch (NoSignedInUserException | UnauthorisedOperationException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, null);
+        }
+    }
+
+    public void undoTurnIn(Project.ProjectStatus newStatus) {
+        try {
+            projectManager.undoTurnIn(getProject().getId(), newStatus);
+        } catch (SQLException | InexistentDatabaseEntityException | InexistentProjectException e) {
+            Exception exception = new SQLException();
+            ErrorDialogFactory.createErrorDialog(exception, frame, null);
+        } catch (IllegalProjectStatusChangeException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, "You can't mark a project with status " + getProject().getStatus().toString() + " as 'In Progress'");
+        } catch (NoSignedInUserException | UnauthorisedOperationException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, null);
+        }
+    }
+
+    public void review(Project.ProjectStatus newStatus) {
+        try {
+            if (newStatus == Project.ProjectStatus.FINISHED) {
+                projectManager.acceptAsFinished(getProject().getId());
+            } else {
+                projectManager.discardTurnIn(getProject().getId(), newStatus);
+            }
+        } catch (SQLException | InexistentDatabaseEntityException | InexistentProjectException e) {
+            Exception exception = new SQLException();
+            ErrorDialogFactory.createErrorDialog(exception, frame, null);
+        } catch (IllegalProjectStatusChangeException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, "You can't mark a project with status " + getProject().getStatus().toString() + " as 'In Progress'");
+        } catch (NoSignedInUserException | UnauthorisedOperationException e) {
+            ErrorDialogFactory.createErrorDialog(e, frame, null);
         }
     }
 }
