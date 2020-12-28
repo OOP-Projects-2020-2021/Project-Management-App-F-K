43f8603102eb5b2893fc6f4465b33b87129ed75b
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-28 19:35:50 +0200

    finish refactoring

--- src/main/java/model/project/exceptions/IllegalProjectStatusChangeException.java
+++ src/main/java/model/project/exceptions/IllegalProjectStatusChangeException.java
@@ -10,7 +10,7 @@ import model.project.Project;
  */
 public class IllegalProjectStatusChangeException extends Exception {
   public IllegalProjectStatusChangeException(
-      Project.ProjectStatus oldStatus, Project.ProjectStatus newStatus) {
+          Project.Status oldStatus, Project.Status newStatus) {
     super(
         "You are not allowed to change the project's status from "
             + oldStatus
