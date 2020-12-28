92e0692d7f9029ccdbd9a7c88b76b50bd816f796
Bori_Fazakas <fazakasbori@gmail.com>
2020-12-28 21:20:28 +0200

    make ProjectListModel non-singleton, and return unmodifiable lists

--- src/main/java/view/project/ProjectListModel.java
+++ src/main/java/view/project/ProjectListModel.java
@@ -6,45 +6,41 @@ import model.project.Project;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * ProjectListMode represents the underlying model containing the list of projects to be displayed.
- * It is implemented using the singleton pattern, so that when the data in the model changes, all of
- * its instances would be updated. When a user applies a filter on the projects list in the
+ * One instance is shared among the ProjectFilterPanel (and its controller) and the corresponding
+ * ProjectTable (and its controller).
+ * When a user applies a filter on the projects list in the
  * ProjectFilterPanel, the model will be updated by the ProjectFilterPanel's controller. The
  * controller which displays the actual projects in a table, listens to the changes made in the
  * model, and updates the view.
  *
- * @author Beata Keresztes
+ * @author Beata Keresztes, Bori Fazakas
  */
 public class ProjectListModel implements PropertyChangeObservable {
 
   private List<Project> projectList;
-  private PropertyChangeSupport support = new PropertyChangeSupport(this);
   public static final String PROJECT_LIST = "Project list";
+
+  private PropertyChangeSupport support = new PropertyChangeSupport(this);
   private static ProjectListModel instance = new ProjectListModel();
 
-  private ProjectListModel() {
+  public ProjectListModel() {
     projectList = new ArrayList<>();
   }
 
-  public static ProjectListModel getInstance() {
-    return instance;
-  }
-
   public List<Project> getProjectList() {
-    return projectList;
-  }
-
-  public boolean isEmptyProjectList() {
-    return projectList == null || projectList.isEmpty();
+    return Collections.unmodifiableList(projectList);
   }
 
   public void setProjectList(List<Project> newProjectList) {
     List<Project> oldProjectList = projectList;
     projectList = newProjectList;
-    support.firePropertyChange(PROJECT_LIST, oldProjectList, projectList);
+    support.firePropertyChange(PROJECT_LIST, Collections.unmodifiableList(oldProjectList),
+            Collections.unmodifiableCollection(projectList));
   }
 
   @Override
