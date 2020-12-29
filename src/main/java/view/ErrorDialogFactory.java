a591ca147480e91aeaba5d6274f866ab5554d445
keresztesbeata <keresztesbeata00@yahoo.com>
2020-12-29 02:21:10 +0200

    restored illegal state change exception

--- src/main/java/view/ErrorDialogFactory.java
+++ src/main/java/view/ErrorDialogFactory.java
@@ -109,7 +109,13 @@ public class ErrorDialogFactory {
 
   private static final String EMPTY_FIELDS_LEFT_MESSAGE =
       "Please fill in all the required information before continuing!";
-
+  /**
+   * Messages displayed to inform the user that the status change requested for a project is
+   * illegal.
+   */
+  private static final String ILLEGAL_PROJECT_STATUS_CHANGE_TITLE = "Illegal status change.";
+  private static final String ILLEGAL_PROJECT_STATUS_CHANGE_MESSAGE =
+          "This change of project status is now allowed";
   /**
    * Messages to warn the user about attempting to delete a member of a team, while that member
    * still has assigned/supervised projects.
@@ -172,6 +178,9 @@ public class ErrorDialogFactory {
     }
     if (exception instanceof EmptyFieldsException) {
       displayEmptyFieldsErrorDialog(frame, message);
+    }
+    if (exception instanceof IllegalProjectStatusChangeException) {
+      displayIllegalStateChangeErrorDialog(frame, message);
     }
     if (exception instanceof IllegalMemberRemovalException) {
       displayIllegalMemberRemovalErrorDialog(frame, message);
@@ -330,4 +339,12 @@ public class ErrorDialogFactory {
             ILLEGAL_TEAM_REMOVAL_TITLE,
             JOptionPane.ERROR_MESSAGE);
   }
+  /** Displays an error message to inform the user that the project status change is illegal. */
+  private static void displayIllegalProjectStatusChangeErrorDialog(Frame frame, String message) {
+      JOptionPane.showMessageDialog(
+              frame,
+              ILLEGAL_STATUS_CHANGE_MESSAGE + "\n" + message,
+              ILLEGAL_STATUS_CHANGE_TITLE,
+              JOptionPane.ERROR_MESSAGE);
+    }
 }
