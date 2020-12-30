dca03519825fdbac6ece677101890ea987aa4c4c
keresztesbeata <keresztesbeata00@yahoo.com>
2020-12-30 23:37:39 +0200

    moved db to resources; created jar - successfully

--- src/main/java/model/database/SqliteDatabaseConnectionFactory.java
+++ src/main/java/model/database/SqliteDatabaseConnectionFactory.java
@@ -17,7 +17,8 @@ public class SqliteDatabaseConnectionFactory {
   private static void connect() {
     try {
       Class.forName("org.sqlite.JDBC");
-      c = DriverManager.getConnection("jdbc:sqlite:project_management_app.db?foreign_keys=on");
+      c = DriverManager.getConnection("jdbc:sqlite::resource:project_management_app"+
+              ".db?foreign_keys=on");
     } catch (ClassNotFoundException | SQLException e) {
       e.printStackTrace();
       System.exit(1);
